4ff1efa8171f85d76244dd8cce348248abaca3cf
zack_zheng <zack_zhengxiyun@163.com>
2022-06-24 19:17:13 +0800

    no message

---
+++ type-challenges/easy-includes/template.ts
@@ -0,0 +1,6 @@
+import type { Equal } from '@type-challenges/utils'
+
+export type Includes<T extends readonly any[], U> = T extends [infer First, ...infer Rest] ?
+  Equal<First, U> extends true ?
+    true : Includes<Rest, U>
+  :false
