fa601b667137acc36d9d7ad977da6acb650ba5cf
zack_zheng <zack_zhengxiyun@163.com>
2022-08-04 22:37:34 +0800

    remove note

--- type-challenges/medium-chainable-options/template.ts
+++ type-challenges/medium-chainable-options/template.ts
@@ -3,9 +3,3 @@ type Chainable<T = {}> = {
             Chainable<{[Key in keyof T as Key extends K ? never : Key]: T[Key]} & {[key in K]: V}>
   get(): T
 }
-
-// type Chainable<T = {}> = {
-//   option<K extends string, V>(key: K extends keyof T ? T[K] extends V ? never : K : K, value: V): 
-//             Chainable<Omit<T, K> & Record<K, V>>
-//   get(): T
-// }
