25cffd3222dd6f1977efc93e00304c9170253c63
zack_zheng <zack_zhengxiyun@163.com>
2022-08-10 22:08:56 +0800

    generics demo codes

---
+++ notes/MyTypeByGenerics.ts
@@ -0,0 +1,13 @@
+interface Backpack<Type> {
+  add: (obj:Type) => void;
+  get: () => Type
+}
+
+// This line is a shortcut to tell TypeScript there is a constant called 'backpack', and to not worry aboout where it came from
+declare const backpack: Backpack<string>;
+
+// object is a string, because we declared it above as the variabke part of Backpack
+const object = backpack.get()
+
+// Since the backpack variable is a string,you can't pass a number to the add function
+backpack.add(23)
