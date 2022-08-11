0ec656acc53e8d9f2619df820e6c48f3ba7b3ca1
zack_zheng <zack_zhengxiyun@163.com>
2022-08-11 22:22:04 +0800

    functions notes

---
+++ notes/Functions_Types.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-console */
+// Parameter type annotation
+function greet(name: string) {
+  console.log(`Hello, ${name.toUpperCase()}!!`)
+}
+
+// Return type annotations
+function getFavoriteNumber(): number {
+  return 26
+}
+
+// No type annotations here, but TypeSCript can spot the bug
+const names = ['Alice', 'Bob', 'Eve']
+
+// Contextual typing for function
+names.forEach((s) => {
+  console.log(s.toUpperCase())
+})
