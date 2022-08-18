9eef56e2b965c359226af16652c0b3acb1a1f756
zack_zheng <zack_zhengxiyun@163.com>
2022-08-18 21:56:35 +0800

    object types

--- notes/Object_Types2.ts
+++ notes/Object_Types2.ts
@@ -10,3 +10,50 @@ function paintShape(opts: PaintOptions) {
 
 const shape = getShape()
 paintShape({shape})
+
+
+// readonly Properties
+interface SomeType {
+  readonly prop: string
+}
+
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+interface ReadonlyPerson {
+  readonly name: string;
+  readonly age: number;
+}
+
+let writablePerson: Person = {
+  name: "Person McPersonface",
+  age: 42
+}
+
+let readonlyPerson: ReadonlyPerson = writablePerson
+console.log(readonlyPerson.age)
+
+
+// Index Signatures
+interface StringArray {
+  [index: number]: string
+}
+
+const myArray: StringArray = getStringArray()
+const secondItem = myArray[1]
+
+
+interface NumberDictionaty {
+  [index: string]: number;
+  length: number;
+  name: string;
+}
+
+interface NumberOrStringDictionary {
+  [index: string]: number | string;
+  length: number;
+  name: string;
+}
