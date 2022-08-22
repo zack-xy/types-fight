447a0746e6ed281c220b942e9ec5760f04835ebd
zack_zheng <zack_zhengxiyun@163.com>
2022-08-22 23:34:34 +0800

    Generics types

--- notes/create_types_Generics.ts
+++ notes/create_types_Generics.ts
@@ -21,3 +21,32 @@ function indentity<Type>(arg: Type): Type {
 
 // 前半段是泛型类型声明，后半段是赋值
 let myIdentity: <Input>(arg: Input) => Input = indentity
+
+
+function indentity2<Type>(arg: Type): Type {
+  return arg
+}
+
+let myIdentity2: { <Type>(arg: Type): Type } = indentity2
+
+
+interface GenericIdentityFn {
+  <Type>(arg: Type): Type
+}
+
+function indentity3<Type>(arg: Type):Type {
+  return arg
+}
+
+
+let myIdentity3: GenericIdentityFn = indentity
+
+interface GenericIndentityFn<Type> {
+  (arg: Type): Type
+}
+
+function indentity4<Type>(arg: Type): Type {
+  return arg;
+}
+
+let myIdentity4: GenericIndentityFn<number> = indentity
