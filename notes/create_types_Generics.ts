5e771610be23119f2cf33e83e4f44cc56456b900
zack_zheng <zack_zhengxiyun@163.com>
2022-08-24 19:14:44 +0800

    add notes

--- notes/create_types_Generics.ts
+++ notes/create_types_Generics.ts
@@ -50,3 +50,74 @@ function indentity4<Type>(arg: Type): Type {
 }
 
 let myIdentity4: GenericIndentityFn<number> = indentity
+
+
+
+interface GenericIdentityFn<Type> {
+  (arg: Type): Type
+}
+
+function identity<Type>(arg: Type): Type {
+  return arg
+}
+
+const myIdentity: GenericIdentityFn<number> = identity
+
+// Generic Classes
+class GenericNumber<NumType> {
+  zeroValue: NumType
+  add: (x: NumType, y: NumType) => NumType
+}
+
+const myGenericNumber = new GenericNumber<number>()
+myGenericNumber.zeroValue = 0
+myGenericNumber.add = function (x, y) {
+  return x + y
+}
+
+// Generic Constraints
+interface LengthWise {
+  length: number
+}
+
+function loggingIdentity<Type extends LengthWise>(arg: Type): Type {
+  console.log(arg.length)
+  return arg
+}
+
+// Using Type Parameters in Generic Constraints
+function getProperty<Type, Key extends keyof Type>(obj: Type, key: Key) {
+  return obj[key]
+}
+
+// Using Class Types in Generics
+function create<Type>(c: { new (): Type }): Type {
+  return new c()
+}
+
+class BeeKeeper {
+  hasMask = true
+}
+
+class ZooKeeper {
+  nametag = 'Mikle'
+}
+
+class Animal {
+  numLegs = 4
+}
+
+class Bee extends Animal {
+  keeper: BeeKeeper = new BeeKeeper()
+}
+
+class Lion extends Animal {
+  keeper: ZooKeeper = new ZooKeeper()
+}
+
+function createInstance<A extends Animal>(c: new() => A): A {
+  return new c()
+}
+
+createInstance(Lion).keeper.nametag
+createInstance(Bee).keeper.hasMask
