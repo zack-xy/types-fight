aa9ec8d299227aab9d49839656b0dd9f67ab4c2a
zack_zheng <zack_zhengxiyun@163.com>
2022-08-25 20:54:22 +0800

    condition types

---
+++ notes/Condition_Types.ts
@@ -0,0 +1,18 @@
+interface IdLabel {
+  id: number
+}
+
+interface NameLabel {
+  name: string
+}
+
+type NameOrId<T extends number | string> = T extends number ? IdLabel : NameLabel
+
+
+function createLabel<T extends number | string>(idOrName: T): NameOrId<T> {
+  throw 'unimplemented'
+}
+
+let a = createLabel('typesctipt')
+let b = createLabel(2.8)
+let c = createLabel(Math.random()? 'hello': 42)
